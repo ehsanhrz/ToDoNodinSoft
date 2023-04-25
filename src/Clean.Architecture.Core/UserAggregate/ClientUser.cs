077864cba683c1b5ece9a74313b1d58f8af65774
ehsan_hrz <ehsanheidarzadeh959@gmail.com>
2023-04-25 17:05:44 +0430

    init

---
+++ src/Clean.Architecture.Core/UserAggregate/ClientUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clean.Architecture.SharedKernel.Interfaces;
+using Clean.Architecture.SharedKernel;
+
+namespace Clean.Architecture.Core.UserAggregate;
+public class ClientUser : EntityBase, IAggregateRoot
+{
+  
+  public string Name { get; set; } = string.Empty;
+
+  public string Password { get; set; } = string.Empty;
+
+  public string Email { get; set; } = string.Empty;
+
+  public string UserName { get; set; } = string.Empty;
+
+  public bool EmailVerfied { get; set; }
+
+  public bool TwoStepLogin { get; set; }
+
+  public string PhoneNumber { get; set; } = string.Empty;
+}
---
+++ src/Clean.Architecture.Core/UserAggregate/Interfaces/IUserSignup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ardalis.Result;
+
+namespace Clean.Architecture.Core.UserAggregate.Interfaces;
+public interface IUserSignup
+{
+  public Task<Result> CheckUserNameUnique(string UserName);
+
+  public Task<Result> CheckPhoneNumberUnique(string PhoneNumber);
+
+  public Task<Result> FireVerficationCodeSenderEvent(ClientUser user);
+
+  public Task<Result> ResetVerficationCodeSenderEvent(ClientUser user);
+
+  public Task<Result> CheckPhoneNumberVerficationCode(ClientUser user, string code);
+
+  public Task<Result> FireCreateUserEvent(ClientUser user);
+  
+}
