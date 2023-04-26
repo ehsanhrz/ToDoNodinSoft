4092eba56ff515c45955a726f1588417021b9aee
ehsan_hrz <ehsanheidarzadeh959@gmail.com>
2023-04-26 19:15:15 +0430

    setting DB Config for new entities.

---
+++ src/Clean.Architecture.Infrastructure/Data/Config/UserAggregateCFG/PhoneValidationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clean.Architecture.Core.UserAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Clean.Architecture.Infrastructure.Data.Config.UserAggregateCFG;
+public class PhoneValidationConfiguration : IEntityTypeConfiguration<PhoneValidation>
+{
+  public void Configure(EntityTypeBuilder<PhoneValidation> builder)
+  {
+    builder.HasKey(p => p.Id);
+
+    builder.Property(p => p.UserID).IsRequired();
+
+    builder.Property(p => p.UserPhoneNumber)
+      .HasMaxLength(12)
+      .IsRequired();
+
+    builder.Property(p => p.Code);
+  }
+}
